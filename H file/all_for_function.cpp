fd9982f0b4a368428d3dc9ea493bc5545a356abf
MahmudAlj <96774345+MahmudAlj@users.noreply.github.com>
2024-05-02 13:45:41 +0300

    up10

--- H file/all_for_function.cpp
+++ H file/all_for_function.cpp
@@ -1,8 +1,9 @@
 #include <iostream>    
 #include "my_algorithm.h"
-
+#include "calculat.cpp"
 int main() {
-    
+    std::cout << plus(1,2) << std::endl;
+
     algo::fora(8,10);
     std::cout << " " << std::endl;
     algo::whilea(6,10);

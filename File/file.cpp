a0ded281487720930d8853338600a01493127caa
MahmudAlj <96774345+MahmudAlj@users.noreply.github.com>
2024-05-07 19:25:17 +0300

    up1

--- File/file.cpp
+++ File/file.cpp
@@ -4,12 +4,13 @@
 
 
 int main(){
-    std::ofstream oMyFile("benimo.txt"); // output file 
-    std::ifstream iMyFile("benimi.txt"); //  input file
-    std::iomanip 
-    iMyFile << "benim seni cok seviyorumfdsfsdfsdf fdsfsdksdjfsjebfkjdkfsdf"; // dosyanın icine yazma
-    oMyFile << "ljsdkjfldjfkjdljfksjflkdjflkjdkljdjf";
-  
+    // std::ofstream oMyFile("benimo.txt"); // output file 
+    // std::ifstream iMyFile("benimi.txt"); // input file
     
+    // iMyFile << "benim seni cok seviyorumfdsfsdfsdf fdsfsdksdjfsjebfkjdkfsdf"; // dosyanın icine yazma
+    // oMyFile << "ljsdkjfldjfkjdljfksjflkdjflkjdkljdjf";
+  
+    std::fstream in_file{"benimo.txt", std::ios::in};
+    std::fstream in_file2{"benimo.txt", std::ios::in | std::ios::binary};
     return 0;  
 }

581c475c9b49c8c86c7adaed445436074c1bfaff
MahmudAlj <96774345+MahmudAlj@users.noreply.github.com>
2024-05-07 17:47:48 +0300

    up1

--- File/in_out.cpp
+++ File/in_out.cpp
@@ -1,8 +1,32 @@
 #include <iostream> 
+#include <iomanip> // must include for manipulators
+
+int main(){
+// //boolean type  1(true)0(false)
+
+// std::cout << (100 == 100) << std::endl; // 1
+// std::cout << (100 == 200) << std::endl; // 0
+
+// std::cout << std::boolalpha;
+// std::cout << (100 == 100) << std::endl; // true 
+// std::cout << (100 == 200) << std::endl; // false 
+
+// std::cout << std::noboolalpha;
+// std::cout << (100 == 100) << std::endl; // 1
+// std::cout << (100 == 200) << std::endl; // 0
+
+// //method version 
+// std::cout.setf(std::ios::boolalpha);
+// std::cout << (100 == 100) << std::endl; // true
+// std::cout << (100 == 200) << std::endl; // false
+
+// // reset to default 
+// std::cout << std::resetiosflags(std::ios::boolalpha);
+// std::cout << (100 == 100) << std::endl; // 1
+// std::cout << (100 == 200) << std::endl; // 0
+ 
 
 
-
-int main(void){
 
 
     return 0;

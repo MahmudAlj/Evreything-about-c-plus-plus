aef50a291d152c8d1b66627d6394f32284636658
MahmudAlj <96774345+MahmudAlj@users.noreply.github.com>
2024-05-17 16:29:23 +0300

    up

---
+++ STl/genel.cpp
@@ -0,0 +1,23 @@
+#include  <iostream>
+
+#define my_age 19
+#define my_weight 78
+#define MAX(a,b) ((a > b) ? a : b)
+
+template <typename T> 
+
+
+
+
+int main(){
+
+int a,b;
+std::cin >> a >> b;
+std::cout << MAX(a,b);
+
+
+
+
+
+    return 0;
+}

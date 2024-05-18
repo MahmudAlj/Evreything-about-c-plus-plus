46646feed0593a15dd900fa4dd8eb4a16e7c68eb
MahmudAlj <96774345+MahmudAlj@users.noreply.github.com>
2024-05-18 15:43:37 +0300

    up

--- STl/genel.cpp
+++ STl/genel.cpp
@@ -5,17 +5,20 @@
 #define MAX(a,b) ((a > b) ? a : b)
 
 template <typename T> 
+T MIN(T a,T b){
+    return ((a < b) ? a : b);
+}
 
 
 
 
 int main(){
 
-int a,b;
-std::cin >> a >> b;
+int a = 10;
+int b = 20;
+
 std::cout << MAX(a,b);
-
-
+std::cout << MIN(a,b);
 
 
 

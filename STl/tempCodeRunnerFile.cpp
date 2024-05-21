063094e6b814e143c08f689b1e75f693d08be55c
MahmudAlj <96774345+MahmudAlj@users.noreply.github.com>
2024-05-21 20:05:54 +0300

    up?

---
+++ STl/tempCodeRunnerFile.cpp
@@ -0,0 +1,38 @@
+std::list <int> mylist;
+
+// mylist.push_back(10);
+// mylist.push_back(20);
+// mylist.push_back(30);
+// mylist.push_front(40);
+
+// for(std::list<int>::iterator it = mylist.begin(); it != mylist.end(); it++){
+//     std::cout << *it << std::endl;
+// }
+
+
+// std::list <int> mypopular = {2,3,4,6,3,2,7,8,9,3,5,7,10};
+// std::list <int> myfirst; // 1,5
+// std::list <int> mysecond; // 6,10
+
+// for (std::list<int>::iterator im = mypopular.begin(); im != mypopular.end(); im++){
+//     int my = *im;
+//     if(my <= 5){
+//         myfirst.push_back(my);
+//     }else{
+//         mysecond.push_back(my);
+//     }
+// }
+
+// for(std::list<int>::iterator it = mypopular.begin(); it != mypopular.end(); it++){
+//     std::cout << *it << " ";
+// }
+// std::cout << std::endl;
+// for(std::list<int>::iterator it = myfirst.begin(); it != myfirst.end(); it++){
+//     std::cout << *it << " ";
+// }
+// std::cout << std::endl;
+// for(std::list<int>::iterator it = mysecond.begin(); it != mysecond.end(); it++){
+//     std::cout << *it << " ";
+// }
+// std::cout << std::endl;
+

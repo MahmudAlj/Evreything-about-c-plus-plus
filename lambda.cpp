c63336b021983f90ee880c6833775ffabdeb27c3
MahmudAlj <96774345+MahmudAlj@users.noreply.github.com>
2024-06-05 16:07:08 +0300

    add new method (stateful)

--- lambda.cpp
+++ lambda.cpp
@@ -5,6 +5,7 @@ int main(){
    // vectorle kullanısı 
    // std::function
    // [] () -> return_type specifiers {};  
+   //[captured_variables] () -> returun_type specifiers { };
 auto l = [] (int x) { std::cout << x << std::endl;};
 auto k = [] (int x, int y) -> int { return x + y;};
 auto j = [] (int x, int y) { return x + y;};
@@ -18,5 +19,7 @@ std::cout << j(30,2) << std::endl;
 [] () {std::cout << "hello world" << std::endl;};
 int x {10};
 [] (int x) {std::cout << x << std::endl;}(100);
+
+
     return 0;
 }

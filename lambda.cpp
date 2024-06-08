7194fcac7cff9816712f3183a01589f74d567400
MahmudAlj <96774345+MahmudAlj@users.noreply.github.com>
2024-06-08 13:43:48 +0300

    add some example for my code

--- lambda.cpp
+++ lambda.cpp
@@ -3,22 +3,26 @@
 
 int main(){
    // vectorle kullanısı 
-   // std::function
-   // [] () -> return_type specifiers {};  
-   //[captured_variables] () -> returun_type specifiers { };
-auto l = [] (int x) { std::cout << x << std::endl;};
-auto k = [] (int x, int y) -> int { return x + y;};
-auto j = [] (int x, int y) { return x + y;};
+//    // std::function
+//    // [] () -> return_type specifiers {};  
+//    //[captured_variables] () -> returun_type specifiers { };
+// auto l = [] (int x) { std::cout << x << std::endl;};
+// auto k = [] (int x, int y) -> int { return x + y;};
+// auto j = [] (int x, int y) { return x + y;};
 
 
-l(10);
-std::cout << k(40,3) << std::endl;
-std::cout << j(30,2) << std::endl;
+// l(10);
+// std::cout << k(40,3) << std::endl;
+// std::cout << j(30,2) << std::endl;
 
 
-[] () {std::cout << "hello world" << std::endl;};
-int x {10};
-[] (int x) {std::cout << x << std::endl;}(100);
+// [] () {std::cout << "hello world" << std::endl;};
+// int x {10};
+// [] (int x) {std::cout << x << std::endl;}(100);
+
+int a = 5;
+auto lambda = [a]() mutable { a = 10; return a; };
+std::cout << a << " " << lambda() << std::endl;
 
 
     return 0;

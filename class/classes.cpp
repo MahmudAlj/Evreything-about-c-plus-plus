d97ce96a141e0ac5f05cd54d5dce9b8a21a6c386
MahmudAlj <96774345+MahmudAlj@users.noreply.github.com>
2024-12-04 18:37:09 +0300

    change some code and add some method

--- class/classes.cpp
+++ class/classes.cpp
@@ -19,34 +19,48 @@ public: // acik value
 
 
 //class
- class ornek{
- private: //classın ıcınde kullanıcının degıstmesını yada sabıt kalmasını ıstedıgımız objeler yada fonksıyonlar yer alır (classın dısında degıstırılemez ve kullanılamaz )
-  int age; 
- std::string name;
- public: // classın ıcınde kullanıcının degıstırılıp ekledıgı objeler (herkese acık)
-  int age2 = 1; 
- std::string name2 = " ";
- ornek(int age2, std::string name2){ //parametrelı constructor ile direkt veri alma
-   std::cout << "constructor calisti" << std::endl;
-   std::cout << age2 << "  " << name2 << std::endl;
- }
-   void al_veri(){
-    std::cin >> age;
-    std::cin >> name;
-   }
-   void yazdir(){
-   std::cout << age2 << " " << name2 << std::endl;
- }
-~ornek(){std::cout << "destructor calisti"<<std::endl;}
- protected: // private ama classın kendısı ve sınıfın alt sınıfları tarafından dogrudan erısılebılır fakat classın dısındakı yapılar tarafından erısım ıznı yoktur
+class ornek{
+    private: //classın ıcınde kullanıcının degıstmesını yada sabıt kalmasını ıstedıgımız objeler yada fonksıyonlar yer alır (classın dısında degıstırılemez ve kullanılamaz )
+        int age; 
+        std::string name;
+    public: // classın ıcınde kullanıcının degıstırılıp ekledıgı objeler (herkese acık)
+        int age2 = 1; 
+        std::string name2 = " ";
+        std::string benimki;
+        std::string benimki1;
+        std::string benimki2;
+      ornek(int age2, std::string name2){ //parametrelı constructor ile direkt veri alma
+          std::cout << "constructor calisti" << std::endl;
+          std::cout << age2 << "  " << name2 << std::endl;
+         
+      }
+      
+      void arabalar(std::string x, std::string y, std::string z){
+        benimki = x;
+        benimki1 = y;
+        benimki2 = z;
+      }
+      
+      int max_yas(int age);
+
+      void al_veri(){
+          std::cin >> age;
+          std::cin >> name;
+      }
+
+      void yazdir(){
+          std::cout << age2 << " " << name2 << std::endl;
+          std::cout << "arabalar :" << benimki  << " " << benimki1 << " " << benimki2 << std::endl;
+      }
+
+      ~ornek(){std::cout << "destructor calisti"<<std::endl;}
+
+    protected: // private ama classın kendısı ve sınıfın alt sınıfları tarafından dogrudan erısılebılır fakat classın dısındakı yapılar tarafından erısım ıznı yoktur
 }; 
 
-class yeni{
-public:
-int be = 10;
-std::string be1 = "mahmud";
-};
-
+int ornek::max_yas(int age) {
+    return (age > 100) ? 100 : age;
+}
 int main(){
     
 ornek bir(18,"code"); // constructor ile direk veri alma 
@@ -55,8 +69,10 @@ ornek bir(18,"code"); // constructor ile direk veri alma
 bir.age2 = 18;  // calısır cuku publicte saklı bır verı(elle verı gırme )
 bir.name2 = "mahmud"; // calısır cuku publicte saklı bır verı
 bir.al_veri(); //fonksıyon yardımıyla kullanıcı tafından verı almak 
+// classın dısında tanımlanan bır metodu cagırma 
+bir.arabalar("cadillac", "porsche", "RAM");
 bir.yazdir(); // fonskıyon ıle alınan verı yazdırma
-
+std::cout << bir.max_yas(99) << std::endl;
 // metod kullanmadan bır objenın ozellıklerını yazdırma 
 std::cout << "metod kullanmadan "<< bir.age2 << " " << bir.name2 << std::endl;
     return 0; // calısan butun destructorlar cagrılır 

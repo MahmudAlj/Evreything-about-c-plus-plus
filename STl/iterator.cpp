0abc62dc89ae1c0bdc766dbd5f5d3f4ec83a2704
MahmudAlj <96774345+MahmudAlj@users.noreply.github.com>
2024-12-18 22:04:03 +0300

    add some thing

--- STl/iterator.cpp
+++ STl/iterator.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <list>
 #include <map>
-#include <deque>
-#include <queue>
 #include <vector>
 #include <set>
+#include <deque>
+#include <queue>
 #include <unordered_map>
 #include <unordered_set>
 #include <forward_list>
 
-
+// iterator dongu ıcın kullanılan bır pointer 
 int main(){
 
 std::list<int> mylist{ 2,3,8,7,1,5,9,6,4,10};
+
+for(std::list<int>::iterator itt = mylist.begin(); itt != mylist.end(); itt++){
+    std::cout << *itt << " ";
+}
+std::cout<< std::endl;
 
 auto it = mylist.begin(); // iterator
 
@@ -20,13 +25,12 @@ std::cout << *it << std::endl; // index 0 = 2
 std::cout << *it + 1 << std::endl; // index 0 + 1 = 3
 
 
-
-std::set<std::string> mystring{"mehmud","mahmud","gokay","sinem"};
+std::set<std::string> mystring{"mehmud","mahmud","gokay","sinem"}; 
 for(std::set<std::string>::iterator it1 = mystring.begin(); it1 != mystring.end(); it1++){
     std::cout << *it1 << std::endl;
 }
 
-std::map<auto> myauto
+
 
 
 
@@ -34,4 +38,12 @@ std::map<auto> myauto
     return 0;
 }
 
+
+/*
+list
+set 
+
+
+
+*/
 

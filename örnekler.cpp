6c688de2f556a2cc347de13a3df81afa51928885
MahmudAlj <96774345+MahmudAlj@users.noreply.github.com>
2025-08-12 17:06:18 +0300

    ss

---
+++ örnekler.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+
+int main (){
+    // std::cout << "hello world" << std::endl;
+
+// int x, y;
+// int toplam = 0;
+
+// std::cin >> x >> y;
+
+// toplam = x + y;
+// std::cout << toplam;
+
+
+// int x ,y ,z;
+// double ortalama = 0.0;
+// std::cin >> x >> y >> z;
+// ortalama  = (x + y + z) / 3.0;
+// std::cout << "Ortalama: " << ortalama << std::endl;
+
+
+// float pi = 3.141592653589793;
+// int cevre;
+// float yaricap;
+// std::cin >> cevre;
+// yaricap = cevre / (2 * pi);
+// std::cout << "Yaricap: " << yaricap << std::endl;
+
+
+// int sayi;
+
+// std::cin >> sayi;
+// if(sayi % 2 == 0) {
+//     std::cout << "Sayi cift" << std::endl;
+// } else {
+//     std::cout << "Sayi tek" << std::endl;
+// }   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
